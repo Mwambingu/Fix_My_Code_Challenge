ff554141ac76a21dcc2b07d90c7fafe948762fb2
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-10-11 16:22:26 +0300

    Hot fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+"""
+Contains a square
+"""
+
 
 class square():
-    
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -19,6 +21,7 @@ class square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
